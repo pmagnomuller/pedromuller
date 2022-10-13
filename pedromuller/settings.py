b59fe3e1d50e37178ea0182927e94f277a813eb6
Pedro Magno Müller <pm.mueller@pragmaticindustries.de>
2022-10-13 11:48:19 +0200

    new resume and adjustments in homepage

--- pedromuller/settings.py
+++ pedromuller/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cwyb=s643pef^c*b8^mg=zfix7gmh72v3#6_*@t#pd9dn3dzh-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','pedromuller.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','pedromuller.herokuapp.com']
 
 
 # Application definition
