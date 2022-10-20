0b5e6708bf4227f17f361c2ca7853948c929c4a0
Pedro Magno Müller <pm.mueller@pragmaticindustries.de>
2022-10-20 17:55:09 +0100

    email uses ssl

--- pedromuller/settings.py
+++ pedromuller/settings.py
@@ -172,8 +172,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_SSL=True
 EMAIL_PORT = env("EMAIL_PORT")
-EMAIL_USE_TLS=True
 EMAIL_HOST_USER = env("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
 
